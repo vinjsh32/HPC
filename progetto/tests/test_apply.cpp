ef516bbac4e2d0e9335cb823333ef9d7defb4138
vinjsh32 <100126289+vinjsh32@users.noreply.github.com>
2025-08-07 22:27:36 +0200

    Add XOR evaluation test

--- progetto/tests/test_apply.cpp
+++ progetto/tests/test_apply.cpp
@@ -16,20 +16,34 @@ TEST(ApplyBasic, AndOrNot)
     OBDD* bdd2 = obdd_create(10, order); /* x9 */
     bdd2->root = obdd_node_create(9, obdd_constant(0), obdd_constant(1));
 
+    OBDD tmp = {nullptr,10,order};
+
     /* (x0 AND x9) */
-    OBDDNode* andRoot = obdd_apply(bdd1, bdd2, 0);
+    OBDDNode* andRoot = obdd_apply(bdd1, bdd2, OBDD_AND);
     int assign[10] = {1,0,0,0,0,0,0,0,0,1};
-    int res = obdd_evaluate(&(OBDD){andRoot,10,order}, assign);
+    tmp.root = andRoot;
+    int res = obdd_evaluate(&tmp, assign);
     ASSERT_EQ(res, 1);
 
     /* OR su (1,1) = 1 */
-    OBDDNode* orRoot = obdd_apply(bdd1, bdd2, 1);
-    res = obdd_evaluate(&(OBDD){orRoot,10,order}, assign);
+    OBDDNode* orRoot = obdd_apply(bdd1, bdd2, OBDD_OR);
+    tmp.root = orRoot;
+    res = obdd_evaluate(&tmp, assign);
     ASSERT_EQ(res, 1);
 
+    /* XOR su (1,0) = 1 */
+    assign[0] = 1;
+    assign[9] = 0;
+    OBDDNode* xorRoot = obdd_apply(bdd1, bdd2, OBDD_XOR);
+    tmp.root = xorRoot;
+    res = obdd_evaluate(&tmp, assign);
+    ASSERT_EQ(res, 1);
+    assign[9] = 1; /* ripristina x9 per i test successivi */
+
     /* NOT(x9) con x9=1 deve dare 0 */
-    OBDDNode* notRoot = obdd_apply(bdd2, NULL, 2);
-    res = obdd_evaluate(&(OBDD){notRoot,10,order}, assign);
+    OBDDNode* notRoot = obdd_apply(bdd2, NULL, OBDD_NOT);
+    tmp.root = notRoot;
+    res = obdd_evaluate(&tmp, assign);
     ASSERT_EQ(res, 0);
 
     obdd_destroy(bdd1);
